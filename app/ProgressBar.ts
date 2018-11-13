1d21200a849480e92685683f5e1ab1a97c0cea0d
Super Edriver <superedriver@gmail.com>
2018-11-13 20:53:37 +0200

    fixed import

--- app/ProgressBar.ts
+++ app/ProgressBar.ts
@@ -1,16 +1,16 @@
-let PB = require('progressbar.js')
+import { Circle } from 'progressbar.js'
 import { bootstrap } from './helpers'
 
 export class ProgressBar {
   public $elem = document.createElement('div')
-  public circle: PB
+  public circle: Circle
 
   constructor(public $root: HTMLElement) {
     this.$elem.id = 'progress-bar'
     this.$elem.classList.add('progress-bar')
     bootstrap(this.$root, this.$elem)
 
-    this.circle = new PB.Circle(this.$elem, {
+    this.circle = new Circle(this.$elem, {
       color: '#0078c8',
       strokeWidth: 4,
       trailColor: '#1c1c1c',
